053e113fd66b64eb52ccbb54c19cac0f4b02865b
Link1515 <h26281660@gmail.com>
2022-05-02 12:41:02 +0800

    props 類型限制

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import './index.scss';
 
 export default class Header extends Component {
+  static propTypes = {
+    addTodo: PropTypes.func.isRequired
+  };
+
   handleKeyUp = (event) => {
     const { target, keyCode } = event;
 
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ListItem from '../ListItem';
 import './index.scss';
 
 export default class List extends Component {
+  static propTypes = {
+    todos: PropTypes.array.isRequired,
+    updateTodoState: PropTypes.func.isRequired
+  };
+
   render() {
     const { todos, updateTodoState } = this.props;
 
